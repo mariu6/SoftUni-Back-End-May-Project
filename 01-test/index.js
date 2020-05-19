6ee577d2df9cde43d3d7b1cde00f6490e7534510
Mariusz Ilijew <mariu6@gmail.com>
2020-05-19 16:17:01 +0200

    for the laptop

--- 01-test/index.js
+++ 01-test/index.js
@@ -1,8 +1,39 @@
+// const http = require('http');
+
+// http.createServer((req, res) => {
+//   res.write('Hi!');
+//   res.end();
+// }).listen(1337);
+
+// console.log('Node.js server running on port 1337');
+// //  http://localhost:1337/
+
+
+
+// const http = require('http');
+// const url = require('url');
+// const port = 1337;
+
+// http.createServer((req, res) => {
+//     let path = url.parse(req['url']).pathname;
+//     if (path === '/') {
+//         // TODO: Send 'Welcome to home page!'
+//         res.write('Welcome to home page!');
+//         res.end();
+//     }
+// }).listen(port);
+// //  http://localhost:1337/
+
+
+
 const http = require('http');
+const port = 3000;
 
 http.createServer((req, res) => {
-  res.write('Hi!');
-  res.end();
-}).listen(1337);
-
-console.log('Node.js server running on port 1337');
+  res.writeHead(200, { // Response Status Code
+    'Content-Type': 'text/plain'
+  }); 
+  res.write('Hello from Node.js'); // UTF-8 Encoding
+  res.end(); // Always End the Response
+}).listen(port);
+//  http://localhost:3000/
