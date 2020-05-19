ec4dcc17fb34f0fa228b3846c4dbd3179a518332
Mariusz Ilijew <mariu6@gmail.com>
2020-05-19 22:49:02 +0200

    some npm modules added

--- 01-test/index.js
+++ 01-test/index.js
@@ -10,30 +10,38 @@
 
 
 
-// const http = require('http');
-// const url = require('url');
-// const port = 1337;
-
-// http.createServer((req, res) => {
-//     let path = url.parse(req['url']).pathname;
-//     if (path === '/') {
-//         // TODO: Send 'Welcome to home page!'
-//         res.write('Welcome to home page!');
-//         res.end();
-//     }
-// }).listen(port);
-// //  http://localhost:1337/
-
-
-
 const http = require('http');
-const port = 3000;
+const url = require('url');
+const port = 1337;
 
 http.createServer((req, res) => {
-  res.writeHead(200, { // Response Status Code
-    'Content-Type': 'text/plain'
-  }); 
-  res.write('Hello from Node.js'); // UTF-8 Encoding
-  res.end(); // Always End the Response
+  let path = url.parse(req['url']).pathname;
+  if (path === '/') {
+    // TODO: Send 'Welcome to home page!'
+    res.write('Welcome to home page!');
+    res.end();
+  } else if (path === '/login') {
+    // TODO: Send 'Welcome to home page!'
+    res.write('Welcome to login page!');
+    res.end();
+  } else {
+    // TODO: Send 'Welcome to home page!'
+    res.write('Error page 404!');
+    res.end();
+  }
 }).listen(port);
-//  http://localhost:3000/
+//  http://localhost:1337/
+
+
+
+// const http = require('http');
+// const port = 3000;
+
+// http.createServer((req, res) => {
+//   res.writeHead(200, { // Response Status Code
+//     'Content-Type': 'text/plain'
+//   }); 
+//   res.write('Hello from Node.js'); // UTF-8 Encoding
+//   res.end(); // Always End the Response
+// }).listen(port);
+// //  http://localhost:3000/
