1036ed5e2d6ff290a8282a7b6dbfaeb1aa2d94b8
Mariusz Ilijew <mariu6@gmail.com>
2020-05-21 22:58:11 +0200

    a

--- 01-test/app.js
+++ 01-test/app.js
@@ -1,3 +1,9 @@
+const express = require("express");          // когато няма ./ значи си взима модул от node_modules; 3-rd party module
+const fs = require("fs");    // node.js  core module, не е депенданси 
+const add = require('./someFuncToRequire');
+
+
+console.log(add(1,2,3));
 console.log("yey!");
 let a = 5;
 let b = "baba";
